7e3f8a190462a4e5cbc5458294984b9db95426c4
djalal <djalal@users.noreply.github.com>
2021-05-20 13:47:00 +0200

    dev package required after all

--- .artifakt/build.sh
+++ .artifakt/build.sh
@@ -11,7 +11,7 @@ echo "------------------------------------------------------------"
 
 # NO SCRIPTS, it breaks the build
 # see https://stackoverflow.com/a/61349991/1093649
-composer --no-ansi --no-interaction update --no-cache --no-progress  --no-autoloader --no-scripts --no-dev
+composer --no-ansi --no-interaction update --no-cache --no-progress  --no-autoloader --no-scripts
 
 chown -R www-data:www-data /var/www/html/
 
