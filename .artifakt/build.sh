788201397bd0c39f77c6560e68dfeb03d4a0535d
djalal <djalal@users.noreply.github.com>
2021-05-21 08:58:23 +0200

    clean

--- .artifakt/build.sh
+++ .artifakt/build.sh
@@ -11,7 +11,8 @@ echo "------------------------------------------------------------"
 
 # NO SCRIPTS, it breaks the build
 # see https://stackoverflow.com/a/61349991/1093649
-composer --no-ansi --no-interaction update --no-cache --no-progress  --no-autoloader --no-scripts
+# not needed anymore, see base image
+# composer --no-ansi --no-interaction update --no-cache --no-progress  --no-autoloader --no-scripts
 
 echo "export APP_ENV=$APP_ENV" >> /etc/apache2/envvars
 echo "export APP_DEBUG=$APP_DEBUG" >> /etc/apache2/envvars
