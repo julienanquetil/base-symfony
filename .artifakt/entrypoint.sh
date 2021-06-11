f3cc2384124876121449a00a1e18b83cfa574346
djalal <djalal@users.noreply.github.com>
2021-06-11 14:50:46 +0200

    no error on existing symlink (#12)

--- .artifakt/entrypoint.sh
+++ .artifakt/entrypoint.sh
@@ -9,9 +9,8 @@ echo "The following build args are available:"
 env
 echo "------------------------------------------------------------"
 
-
 mkdir -p /data/var/log /data/var/uploads /data/var/cache && \
-  ln -s /data/var /var/www/html/var && \
+  ln -sfn /data/var /var/www/html/var  && \
   chown -R www-data:www-data /var/www/html/ /data/var
 
 echo ">>>>>>>>>>>>>> END CUSTOM ENTRYPOINT SCRIPT <<<<<<<<<<<<<<<<< "
