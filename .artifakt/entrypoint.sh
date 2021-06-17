5ae4ff86e7f1d76a436064fdf290f5811799fd46
djalal <djalal@users.noreply.github.com>
2021-06-17 14:27:09 +0200

    fix slow perf risk (#18)
    * fix slow perf risk
    
    Signed-off-by: djalal <djalal@users.noreply.github.com>
    
    * chown created folders

--- .artifakt/entrypoint.sh
+++ .artifakt/entrypoint.sh
@@ -11,6 +11,6 @@ echo "------------------------------------------------------------"
 
 mkdir -p /data/var/log /data/var/uploads /data/var/cache && \
   ln -sfn /data/var /var/www/html/var  && \
-  chown -R www-data:www-data /var/www/html/ /data/var
+  chown www-data:www-data /data/var/log /data/var/uploads /data/var/cache
 
 echo ">>>>>>>>>>>>>> END CUSTOM ENTRYPOINT SCRIPT <<<<<<<<<<<<<<<<< "
