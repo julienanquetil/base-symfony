ecd11c4fd73434e54498bb14a977c807014d8040
djalal <djalal@users.noreply.github.com>
2021-07-26 10:20:57 +0200

    remove env block (#27)

--- .artifakt/entrypoint.sh
+++ .artifakt/entrypoint.sh
@@ -4,11 +4,6 @@ set -e
 
 echo ">>>>>>>>>>>>>> START CUSTOM ENTRYPOINT SCRIPT <<<<<<<<<<<<<<<<< "
 
-echo "------------------------------------------------------------"
-echo "The following build args are available:"
-env
-echo "------------------------------------------------------------"
-
 mkdir -p /data/var/log /data/var/uploads /data/var/cache && \
   ln -s /data/var /var/www/html/var && \
   chown www-data:www-data /data/var/log /data/var/uploads /data/var/cache 
